db3d259b77ee11bf880da3ae6ea2ffbe936eea13
yujianping <943245255@qq.com>
2019-12-25 15:40:45 +0800

    add index and put bootsstrap

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -3,15 +3,14 @@ package life.majiang.community.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping(value="/")
+    public String retu(){
+        return "home";
     }
 }
